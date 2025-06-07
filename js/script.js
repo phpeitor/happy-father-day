c120c5aefefd1f0d87aa7728f0d2f2ed661406c5
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-06-07 00:31:19 -0500

    images commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const lines = [
         "Querido Papá,",
-        "Recuerdo todas esas noches en las que trabajabas",
-        "hasta tarde, no solo para cumplir con tus proyectos,",
-        "sino también para asegurarte de que tu familia tuviera",
-        "todo lo que necesitaba.<br/><br/><br/>",
-        "Eres nuestro 'debugger' personal siempre encontrando",
-        "formas de resolver cualquier problema que se nos",
-        "presente. Espero que hoy puedas desconectar un",
-        "poco y disfrutar de tu día.",
-        "Con todo mi cariño,",
-        "♥PHPeitor"
+        "Recuerdo con cariño esas largas noches en las que,",
+        "a pesar del cansancio, seguías trabajando.",
+        "No solo por cumplir con tus responsabilidades,",
+        "sino por el amor inquebrantable hacia la familia.",
+        "Eres nuestro héroe particular, ese 'debugger' de la vida",
+        "que siempre encuentra la solución perfecta",
+        "a cualquier problema que se presente.",
+        "Deja a un lado el teclado y disfruta como te lo mereces.",
+        "Con todo mi cariño",
+        "♥ PHPeitor",
+        "<a href='./photos' style='color:rgba(230, 12, 12, 0.8);'>see photos</a>"
     ];
 
     let currentLine = 0;
 
     function typeNextLine() {
         if (currentLine < lines.length) {
-            new Typed(`#line${currentLine + 1}`, {
+            const targetId = `line${currentLine + 1}`;
+            const element = document.getElementById(targetId);
+            
+            if (currentLine === 4 || currentLine === 7) {
+                element.classList.add("spaced-line");
+            }
+            
+            new Typed(`#${targetId}`, {
                 strings: [lines[currentLine]],
                 typeSpeed: 30,
                 showCursor: false,
